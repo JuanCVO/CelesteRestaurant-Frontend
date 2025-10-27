f49b07bbe74fc653210cf9226b7493437602375f
JuanCVO <velezospinajuancamilo@gmail.com>
2025-10-27 00:43:09 -0500

    small error with addressing call

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,6 @@
 // lib/auth.ts
 export async function loginUser(email: string, password: string) {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/auth/login`, {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}auth/login`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ email, password }),
